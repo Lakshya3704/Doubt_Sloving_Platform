58ecb85c48395db990499c863c0c3afe0d99b412
Lakshya Agarwal <alakshya722@gmail.com>
2026-02-24 14:01:43 +0530

    final commit

--- BackeEnd/src/index.js
+++ BackeEnd/src/index.js
@@ -1,12 +1,28 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv/config";
 
+// import connectDB from "./db/database.js";
+// import { app } from "./app.js";
+
+// dotenv.config({
+//   path: "/.env",
+// });
+
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("MONGO db connection failed !!!! ", err);
+//   });
+
+
+import "dotenv/config";  // auto-loads .env from project root
 import connectDB from "./db/database.js";
 import { app } from "./app.js";
 
-dotenv.config({
-  path: "/.env",
-});
-
+// No need to call dotenv.config() again
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
